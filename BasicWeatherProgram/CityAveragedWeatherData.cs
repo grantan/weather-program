a9110d6df92b89aa06ebf62bbfceb1b4f16b2b97
Andrew Grant <grantandyt@gmail.com>
2017-10-10 18:05:02 -0600

    initial checkin of weatherprogram code.

---
+++ BasicWeatherProgram/CityAveragedWeatherData.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace BasicWeatherProgram
+{
+    public class CityAveragedWeatherData
+    {
+		public string State { get; set; }
+		public string City { get; set; }
+		public decimal AverageHighTemp { get; set; }
+		public decimal AverageLowTemp { get; set; }
+	}
+}
---
+++ BasicWeatherProgram/WeatherData.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace BasicWeatherProgram
+{
+    public class WeatherData
+    {
+		public string State { get; set; }
+		public string City { get; set; }
+		public DateTime Date { get; set; }
+		public decimal HighTemp { get; set; }
+		public decimal LowTemp { get; set; }
+	}
+}
